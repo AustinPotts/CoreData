8200eb7e92ee53662f7add34d1022ab33ee66ade
AustinPotts <pottsevolvements@gmail.com>
2019-09-16 12:54:08 -0400

    Configured CoreData Subclass File

---
+++ Tasks Starter/Tasks/Model/Task+Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  Task+Convenience.swift
+//  Tasks
+//
+//  Created by Austin Potts on 9/16/19.
+//  Copyright © 2019 Andrew R Madsen. All rights reserved.
+//
+
+import Foundation
+
+//Core data already created the Task class, so we just want to add some functionality to it
+extension Task {
+    
+}
